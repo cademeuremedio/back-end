80784ae34ea9c21bffd009a1bd901ddf824dd512
Rafael <rafael.vasel@gmail.com>
2018-08-24 09:55:58 -0300

    1.01 - antes  do mongodb

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# backend
-Backend do projeto "Cadê o meu remédio", apresentado no Hackfest 2018, realizado em João Pessoa.
+# Backend do projeto "Cadê o meu remédio", apresentado no Hackfest 2018, realizado em João Pessoa.
+Maiores detalhes sobre o evento podem ser encontradas em: http://hackfest.com.br/ . 
 
-Maiores detalhes sobre o evento podem ser enocontradas em: http://hackfest.com.br/
+O site do projeto é o https://www.cademeuremedio.com.br/. 
 
-O site do projeto é o https://www.cademeuremedio.com.br/
+Um vídeo explicativo sobre o proejto está disponível em: https://goo.gl/wkQsUz
+
+O branch master desse repositório está linkado, graças ao apoio do Daniel Fireman, com um deploy automático no Heroku, em https://cademeuremedio-v1.herokuapp.com/
+
+Os nomes comerciais dos medicamentos foram obtidos em: https://github.com/aspto/base-de-dados-de-medicamentos . Obrigado, Stephan Garcia por ter encontrado esse repositório e por todo o apoio que nos deu. 
+
+A base de medicamentos disponíveis nos postos de saúde foi obtida em: http://portalms.saude.gov.br/assistencia-farmaceutica/medicamentos-rename
+
+Você pode ajudar melhorando o código ou divulgando o projeto. Contribuições de código são bem vindas! 
--- server_cademeuremedio.py
+++ server_cademeuremedio.py
@@ -20,20 +20,25 @@ def raiz():
 @app.route('/lista/<termo>')
 @cross_origin()
 def lista_medicamentos_sus(termo):
-    return funcoes_cademeuremedio.lista_medicamentos_sus(termo).to_json(orient='records')
-    # return "Hello lista!"
-
+    if not termo:
+        return '/lista/<termo>'
+    else:
+        return funcoes_cademeuremedio.lista_medicamentos_sus(termo).to_json(orient='records')
 
 @app.route('/reporta_falta/<cod_posto>/<cod_medicamento>/<cod_municipio>')
+@app.route('/reporta_falta/<cod_posto>/<cod_medicamento>/<cod_municipio>/<id_usr>')
 @cross_origin()
-def reporta_falta(cod_posto, cod_medicamento, cod_municipio):
+def reporta_falta(cod_posto, cod_medicamento, cod_municipio, id_usr=''):
+    if not cod_posto:
+        return '/reporta_falta/<cod_posto>/<cod_medicamento>/<cod_municipio>  ou <br> /reporta_falta/<cod_posto>/<cod_medicamento>/<cod_municipio>/<id_usr>'
     return str(funcoes_cademeuremedio.grava_falta_remedio_por_municipio(cod_posto, cod_medicamento, cod_municipio))
 
 
 # esse "proxy" é necessário porque a API do TCU não está em https.
+@app.route('/estabelecimentos/latitude/<latitude>/longitude/<longitude>')
 @app.route('/estabelecimentos/latitude/<latitude>/longitude/<longitude>/raio/<raio>')
 @cross_origin()
-def estabelecimentos(latitude, longitude, raio):
+def estabelecimentos(latitude, longitude, raio=50):
     r = requests.get(
         'http://mobile-aceite.tcu.gov.br/mapa-da-saude/rest/estabelecimentos/latitude/' + latitude + '/longitude/' + longitude + '/raio/' + raio + '?categoria=POSTO%20DE%20SA%C3%9ADE')
     # r = requests.get('http://mobile-aceite.tcu.gov.br/mapa-da-saude/rest/estabelecimentos/latitude/-27.5926371/longitude/-48.5576378/raio/50?categoria=POSTO%20DE%20SA%C3%9ADE')
@@ -46,9 +51,11 @@ def score(cod_posto, cod_medicamento, cod_municipio):
     return '{"resultado":"' + str(funcoes_cademeuremedio.score_posto(cod_posto, cod_medicamento, cod_municipio)) + '"}'
 
 
+@app.route('/ranking')
 @app.route('/ranking/<qtde>')
+@app.route('/ranking/<cidade>/<qtde>')
 @cross_origin()
-def ranking(qtde):
+def ranking(qtde=10):
     return funcoes_cademeuremedio.ranking(qtde)
 
 
@@ -60,4 +67,4 @@ def todos_remedios(termo):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7777))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
